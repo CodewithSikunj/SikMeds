8df83c22e39f20c168fea853a0824a3e3fcf71e0
CodewithSikunj <sikunjyadav@gmail.com>
2025-12-06 11:10:09 +0530

    Rename toggleBtn to Btn in script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-const toggleBtn = document.getElementById("theme-toggle");
+const Btn = document.getElementById("theme-toggle");
 
-// Load saved mode
+
 if (localStorage.getItem("darkMode") === "enabled") {
     document.body.classList.add("dark-mode");
 }
 
-// Toggle dark mode
-toggleBtn.addEventListener("click", () => {
+Btn.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 
     if (document.body.classList.contains("dark-mode")) {
